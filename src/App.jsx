621d952ea7707b28d83b126b2fe1a34845f401c1
sohilaesaam60@gmail.com <sohilaesaam60@gmail.com>
2024-07-30 23:27:47 +0300

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, createHashRouter, RouterProvider} from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import StartFrameWork from './components/SrartFrameWork/StartFrameWork'
 import About from './components/About/About'
@@ -12,7 +12,7 @@ import Portfolio from './components/Portfolio/Portfolio'
 import Contact from './components/Contact/Contact'
 import NotFound from './components/NotFound/NotFound'
 
-const route = createBrowserRouter([
+const route = createHashRouter([
   {path: '/' , element: <Layout/> , children: [
     {path: '/' , element: <StartFrameWork />},
     {path: '/about' , element: <About />},
